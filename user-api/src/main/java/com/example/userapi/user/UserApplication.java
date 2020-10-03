db7e6bb8c57187726f5e9f7cefe827120ae164f2
lunnik9 <arnrbey@mail.ru>
2020-10-03 21:07:33 +0600

    feat: eureka service discovery

--- user-api/src/main/java/com/example/userapi/user/UserApplication.java
+++ user-api/src/main/java/com/example/userapi/user/UserApplication.java
@@ -1,13 +1,51 @@
 package com.example.userapi.user;
 
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class UserApplication {
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+public class BookCatalogApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(3000);
+
+//		CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+//
+//		credentialsProvider.setCredentials(AuthScope.ANY,
+//				new UsernamePasswordCredentials("rest-client", "p@ssword"));
+//
+//		HttpClient client = HttpClientBuilder
+//				.create()
+//				.setDefaultCredentialsProvider(credentialsProvider)
+//				.build();
+//
+//		requestFactory.setHttpClient(client);
+
+        return new RestTemplate(requestFactory);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(BookCatalogApplication.class, args);
     }
+
 }
